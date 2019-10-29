a2d1df51ac910e6808a9cfa81b7c8114d0bfb39c
Potato Starch <57136615+potato-starch-1450@users.noreply.github.com>
2019-10-30 05:14:36 +1450

    extract icons

---
+++ shell-app/_misc/702.js
@@ -0,0 +1,119 @@
+module.exports = function(g, r, i, a, m, e, d) {
+  "use strict";
+  Object.defineProperty(e, "__esModule", {
+      value: !0
+  }), Object.defineProperty(e, "AntDesign", {
+      enumerable: !0,
+      get: function() {
+          return t.default
+      }
+  }), Object.defineProperty(e, "Entypo", {
+      enumerable: !0,
+      get: function() {
+          return n.default
+      }
+  }), Object.defineProperty(e, "EvilIcons", {
+      enumerable: !0,
+      get: function() {
+          return u.default
+      }
+  }), Object.defineProperty(e, "Feather", {
+      enumerable: !0,
+      get: function() {
+          return o.default
+      }
+  }), Object.defineProperty(e, "FontAwesome", {
+      enumerable: !0,
+      get: function() {
+          return c.default
+      }
+  }), Object.defineProperty(e, "FontAwesome5", {
+      enumerable: !0,
+      get: function() {
+          return f.default
+      }
+  }), Object.defineProperty(e, "Foundation", {
+      enumerable: !0,
+      get: function() {
+          return l.default
+      }
+  }), Object.defineProperty(e, "Ionicons", {
+      enumerable: !0,
+      get: function() {
+          return b.default
+      }
+  }), Object.defineProperty(e, "MaterialCommunityIcons", {
+      enumerable: !0,
+      get: function() {
+          return y.default
+      }
+  }), Object.defineProperty(e, "MaterialIcons", {
+      enumerable: !0,
+      get: function() {
+          return p.default
+      }
+  }), Object.defineProperty(e, "Octicons", {
+      enumerable: !0,
+      get: function() {
+          return O.default
+      }
+  }), Object.defineProperty(e, "SimpleLineIcons", {
+      enumerable: !0,
+      get: function() {
+          return j.default
+      }
+  }), Object.defineProperty(e, "Zocial", {
+      enumerable: !0,
+      get: function() {
+          return P.default
+      }
+  }), Object.defineProperty(e, "createMultiStyleIconSet", {
+      enumerable: !0,
+      get: function() {
+          return s.default
+      }
+  }), Object.defineProperty(e, "createIconSet", {
+      enumerable: !0,
+      get: function() {
+          return I.default
+      }
+  }), Object.defineProperty(e, "createIconSetFromFontello", {
+      enumerable: !0,
+      get: function() {
+          return F.default
+      }
+  }), Object.defineProperty(e, "createIconSetFromIcoMoon", {
+      enumerable: !0,
+      get: function() {
+          return M.default
+      }
+  }), Object.defineProperty(e, "createMu", {
+      enumerable: !0,
+      get: function() {
+          return M.default
+      }
+  });
+  var t = S(r(d[0])),
+      n = S(r(d[1])),
+      u = S(r(d[2])),
+      o = S(r(d[3])),
+      c = S(r(d[4])),
+      f = S(r(d[5])),
+      l = S(r(d[6])),
+      b = S(r(d[7])),
+      y = S(r(d[8])),
+      p = S(r(d[9])),
+      O = S(r(d[10])),
+      j = S(r(d[11])),
+      P = S(r(d[12])),
+      s = S(r(d[13])),
+      I = S(r(d[14])),
+      F = S(r(d[15])),
+      M = S(r(d[16]));
+
+  function S(t) {
+      return t && t.__esModule ? t : {
+          default: t
+      }
+  }
+}
